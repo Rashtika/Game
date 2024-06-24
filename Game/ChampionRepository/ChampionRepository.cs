72c8153afb87810bf11ba57b85213cc54d146d9c
barbara <bstimac9009@gmail.com>
2024-06-24 11:38:55 +0200

    fixed champion repository

--- Game/ChampionRepository/ChampionRepository.cs
+++ Game/ChampionRepository/ChampionRepository.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using Npgsql;
-using Game.Common;
+﻿using Game.Common;
 using Game.Model;
 using Game.Repository.Common;
-using System.Numerics;
+using Npgsql;
 
 namespace Game.Repository
 {
@@ -29,20 +23,49 @@ namespace Game.Repository
         public async Task UpdateChampion(Champion champion)
         {
             await using var connection = new NpgsqlConnection(connectionString);
-            var cmdText = "UPDATE \"Champion\" SET \"Name\" = @Name, \"InventoryId\" = @InventoryId, \"Inventory\" = @Inventory, \"IsActive\" = @IsActive, \"DateCreated\" = @DateCreated, \"CreatedByUserId\" = @CreatedByUserId, \"UpdatedByUserId\" = @UpdatedByUserId WHERE \"Id\" = @Id;";
             await connection.OpenAsync();
-            await using var command = new NpgsqlCommand(cmdText, connection);
 
-            command.Parameters.AddWithValue("@Id", champion.Id);
-            command.Parameters.AddWithValue("@Name", champion.Name);
-            command.Parameters.AddWithValue("@Inventory", champion.Inventory);
-            command.Parameters["@InventoryId"].NpgsqlDbType = NpgsqlTypes.NpgsqlDbType.Uuid;
-            command.Parameters.AddWithValue("@IsActive", champion.IsActive);
-            command.Parameters.AddWithValue("@DateCreated", champion.DateCreated);
-            command.Parameters.AddWithValue("@CreatedByUserId", champion.CreatedByUserId);
-            command.Parameters.AddWithValue("@UpdatedByUserId", champion.UpdatedByUserId);
+            // Check if the InventoryId exists in the Inventory table
+            var inventoryExistsCmdText = "SELECT COUNT(*) FROM \"Inventory\" WHERE \"Id\" = @InventoryId";
+            await using (var inventoryExistsCmd = new NpgsqlCommand(inventoryExistsCmdText, connection))
+            {
+                inventoryExistsCmd.Parameters.AddWithValue("@InventoryId", champion.InventoryId);
+                var inventoryCount = (long)await inventoryExistsCmd.ExecuteScalarAsync();
+                if (inventoryCount == 0)
+                {
+                    // Inventory doesn't exist, create a new Inventory record
+                    var newInventoryId = Guid.NewGuid();
 
-            await command.ExecuteNonQueryAsync();
+                    string inventorySql = "INSERT INTO \"Inventory\" (\"Id\", \"IsActive\", \"DateCreated\", \"CreatedByUserId\", \"UpdatedByUserId\") VALUES (@Id, @IsActive, @DateCreated, @CreatedByUserId, @UpdatedByUserId)";
+                    using (NpgsqlCommand inventoryCmd = new NpgsqlCommand(inventorySql, connection))
+                    {
+                        inventoryCmd.Parameters.AddWithValue("@Id", newInventoryId);
+                        inventoryCmd.Parameters.AddWithValue("@IsActive", champion.Inventory.IsActive);
+                        inventoryCmd.Parameters.AddWithValue("@DateCreated", champion.Inventory.DateCreated);
+                        inventoryCmd.Parameters.AddWithValue("@CreatedByUserId", champion.Inventory.CreatedByUserId);
+                        inventoryCmd.Parameters.AddWithValue("@UpdatedByUserId", champion.Inventory.UpdatedByUserId);
+                        await inventoryCmd.ExecuteNonQueryAsync();
+                    }
+
+                    // Update Champion with the new InventoryId
+                    champion.InventoryId = newInventoryId;
+                }
+            }
+
+            // Update the Champion record
+            var cmdText = "UPDATE \"Champion\" SET \"Name\" = @Name, \"InventoryId\" = @InventoryId, \"IsActive\" = @IsActive, \"DateCreated\" = @DateCreated, \"CreatedByUserId\" = @CreatedByUserId, \"UpdatedByUserId\" = @UpdatedByUserId WHERE \"Id\" = @Id;";
+            await using (var command = new NpgsqlCommand(cmdText, connection))
+            {
+                command.Parameters.AddWithValue("@Id", champion.Id);
+                command.Parameters.AddWithValue("@Name", champion.Name);
+                command.Parameters.AddWithValue("@InventoryId", champion.InventoryId);
+                command.Parameters.AddWithValue("@IsActive", champion.IsActive);
+                command.Parameters.AddWithValue("@DateCreated", champion.DateCreated);
+                command.Parameters.AddWithValue("@CreatedByUserId", champion.CreatedByUserId);
+                command.Parameters.AddWithValue("@UpdatedByUserId", champion.UpdatedByUserId);
+
+                await command.ExecuteNonQueryAsync();
+            }
         }
 
         public async Task<IEnumerable<Champion>> GetAll()
@@ -94,10 +117,9 @@ namespace Game.Repository
                     DateCreated = reader.GetDateTime(reader.GetOrdinal("DateCreated")),
                     CreatedByUserId = reader.GetInt32(reader.GetOrdinal("CreatedByUserId")),
                     UpdatedByUserId = reader.GetInt32(reader.GetOrdinal("UpdatedByUserId"))
-                   
+
                 };
             }
-
             return null;
         }
 
@@ -111,13 +133,26 @@ namespace Game.Repository
             await using var connection = new NpgsqlConnection(connectionString);
             await connection.OpenAsync();
 
-            var sql = "INSERT INTO \"Champion\" (\"Id\",\"Name\", \"InventoryId\", \"Inventory\", \"IsActive\", \"DateCreated\", \"CreatedByUserId\", \"UpdatedByUserId\") VALUES (@Id, @Name, @InventoryId, @Inventory, @IsActive, @DateCreated, @CreatedByUserId, @UpdatedByUserId);";
+            // Insert the Inventory first
+            string inventorySql = "INSERT INTO \"Inventory\" (\"Id\", \"IsActive\", \"DateCreated\", \"CreatedByUserId\", \"UpdatedByUserId\") VALUES (@Id, @IsActive, @DateCreated, @CreatedByUserId, @UpdatedByUserId)";
+            Guid newInventoryId = Guid.NewGuid();
+            using (NpgsqlCommand inventoryCmd = new NpgsqlCommand(inventorySql, connection))
+            {
+                inventoryCmd.Parameters.AddWithValue("@Id", newInventoryId);
+                inventoryCmd.Parameters.AddWithValue("@IsActive", champion.Inventory.IsActive);
+                inventoryCmd.Parameters.AddWithValue("@DateCreated", champion.Inventory.DateCreated);
+                inventoryCmd.Parameters.AddWithValue("@CreatedByUserId", champion.Inventory.CreatedByUserId);
+                inventoryCmd.Parameters.AddWithValue("@UpdatedByUserId", champion.Inventory.UpdatedByUserId);
+                await inventoryCmd.ExecuteNonQueryAsync();
+            }
+
+            // Insert the Champion with the InventoryId
+            var sql = "INSERT INTO \"Champion\" (\"Id\", \"Name\", \"InventoryId\", \"IsActive\", \"DateCreated\", \"CreatedByUserId\", \"UpdatedByUserId\") VALUES (@Id, @Name, @InventoryId, @IsActive, @DateCreated, @CreatedByUserId, @UpdatedByUserId);";
 
             await using var command = new NpgsqlCommand(sql, connection);
             command.Parameters.AddWithValue("@Id", champion.Id);
             command.Parameters.AddWithValue("@Name", champion.Name);
-            command.Parameters.AddWithValue("InventoryId", champion.InventoryId);
-            command.Parameters.AddWithValue("@Inventory", champion.Inventory);
+            command.Parameters.AddWithValue("@InventoryId", newInventoryId);
             command.Parameters.AddWithValue("@IsActive", champion.IsActive);
             command.Parameters.AddWithValue("@DateCreated", champion.DateCreated);
             command.Parameters.AddWithValue("@CreatedByUserId", champion.CreatedByUserId);
